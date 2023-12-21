62f8f5e7203fec3bd14dac0016df4bb20a185de8
EmreAktas <emrkts37@gmail.com>
2023-12-21 23:55:35 +0300

    proje phonebook

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "----your--GitHub-Name-------"
+}
+
+variable "git-token" {
+  default = "--------your-GitHub-Token-----"
+}
+
+variable "key-name" {
+  default = "-------Your-KeyName----------"
+}
+
+variable "hosted-zone" {
+  default = "----you-Domain-Name-----"
+}
